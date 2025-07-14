50a98a398688fff4e774246a32a30f2562e9428a
Suraj Kumar <surajkumar@Mac.bbrouter>
2025-07-14 22:16:59 +0530

    changed backend url

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://edutalks.onrender.com/api',
+  baseURL: 'https://edutalks-1.onrender.com/api',
   // baseURL: 'http://localhost:5001/api', // Update this if backend is hosted
 });
 
